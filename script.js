c88f70242d5066a70872b1920794c09293a78493
SSin <srysin26@gmail.com>
2026-01-15 12:01:15 +0530

    worm to snake

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 let head = { x: 300, y: 300 };
-const radius = 5;
-const size = [];
-const count = 100;
 
+const size = [];
+const count = 100;        
 let minSize = 1;
-let maxSize = 40;
+let maxSize = 10;
 const n = (count-1)/2;
 for(let i=0; i<count; ++i){
     let t = 1 - Math.abs(n - i) / n;
@@ -18,12 +17,17 @@ for(let i=0; i<count; ++i){
 }
 
 let circles = [];
+let left = [];
+let right = [];
 
 for (let i = 0; i < count; i++) {
     circles.push({ x: head.x, y: head.y });
 }
 
 function calcCircles(){
+    left.length = 0;
+    right.length = 0;
+
     circles[0].x = head.x;
     circles[0].y = head.y;
     for(let i=1; i<count; ++i){
@@ -38,26 +42,37 @@ function calcCircles(){
             dx /= len;
             dy /= len;
         }
+        
+        let nx = -dy;
+        let ny = dx;
 
         curr.x = prev.x+dx*size[i];
         curr.y = prev.y+dy*size[i];
-        //curr.x = prev.x+dx*radius;
-        //curr.y = prev.y+dy*radius;
+        
+        right.push({x: curr.x - nx * size[i], y: curr.y - ny * size[i]});
+        left.push({x: curr.x + nx * size[i], y: curr.y + ny * size[i]});
     }
 }
 
 function drawCircles(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-
-    for(let i=0; i<count; ++i){
-        ctx.beginPath();
-        ctx.arc(circles[i].x, circles[i].y, size[i], 0, 2 * Math.PI);
-        //ctx.arc(circles[i].x, circles[i].y, radius, 0, 2 * Math.PI);
-        ctx.stroke();
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    ctx.beginPath();
+    ctx.moveTo(left[0].x, left[0].y);
+    for (let p of left) {
+        ctx.lineTo(p.x, p.y);
     }
+    for (let i = right.length - 1; i >= 0; i--) {
+        ctx.lineTo(right[i].x, right[i].y);
+    }
+    ctx.fillStyle = "#88cc88";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
 }
 
 function frame(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     calcCircles();
     drawCircles();
     canvas.addEventListener("mousemove", e => {
